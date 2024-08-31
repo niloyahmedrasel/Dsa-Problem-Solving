83a82164adf9eb63443f404993042a677b78bd6a
Niloy Ahmed Rasel <136223020+niloyahmedrasel@users.noreply.github.com>
2024-08-31 23:27:52 +0600

    Update right view of binary tree.cpp

--- Binary Tree/problem solving/right view of binary tree.cpp
+++ Binary Tree/problem solving/right view of binary tree.cpp
@@ -37,3 +37,28 @@ public:
         return ans;
     }
 };
+
+//using recursion
+class Solution {
+public:
+    void right(TreeNode* root,int level,vector<int> &ans){
+        
+       if(!root){
+        return ;
+       }
+       if(level == ans.size()){
+        ans.push_back(root->val);
+       }
+
+       right(root->right,level+1,ans);
+       right(root->left,level+1,ans);
+    }
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int>ans;
+        if(!root){
+            return ans;
+        }
+        right(root,0,ans);
+        return ans;
+    }
+};
